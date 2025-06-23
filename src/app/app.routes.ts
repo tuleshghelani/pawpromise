ff8da13c358b6379a0a1b056d1bf0d7551eddae0
tuleshghelani <ghelanitulesh@gmail.com>
2025-06-23 16:23:25 +0530

    Enable prelaunch route in app routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,9 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
   },
-  // {
-  //   path: 'prelaunch',
-  //   loadComponent: () => import('./pages/prelaunch/prelaunch.component').then(m => m.PrelaunchComponent)
-  // },
+  {
+    path: 'prelaunch',
+    loadComponent: () => import('./pages/prelaunch/prelaunch.component').then(m => m.PrelaunchComponent)
+  },
   // ... other routes
 ];
